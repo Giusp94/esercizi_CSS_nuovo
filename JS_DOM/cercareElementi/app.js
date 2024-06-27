d43db719a5110d9fcd11726a5bb305dc8b1f69dd
Giuseppe Incurvati <g.incurvati.c@gmail.com>
2024-06-27 16:54:22 +0200

    Added ex Cercare elementi nel DOM, Manipolazione del DOM, Manipolazione del DOM 2

---
+++ JS_DOM/cercareElementi/app.js
@@ -0,0 +1,11 @@
+// Tutti gli elementi con classe "paragrafo".
+const allP = document.getElementsByClassName("paragrafo");
+console.log(allP);
+
+// Cerca l'elemento con id "elemento-3"
+const elementThree = document.getElementById("elemento-3");
+console.log(elementThree);
+
+// Cerca tutti gli elementi h2 della pagina
+const allH2 = document.querySelectorAll("h2");
+console.log(allH2);
